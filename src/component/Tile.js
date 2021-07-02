ed97002b4dc8640dd6599a255e843bcfea3c2e68
YoonSung Kim <opqudk@gmail.com>
2021-07-02 10:55:37 +0900

    점수까지 완성

--- src/component/Tile.js
+++ src/component/Tile.js
@@ -4,7 +4,8 @@ import cn from 'classnames'
 export default function Tile({ id, x, y, value, isMerged, isNew }) {
   return (
     <div className={cn(`tile tile-${value} tile-position-${x}-${y}`, {
-      ['tile-merged']: isMerged, 
+      // ['tile-merged']: isMerged, // class 배열로도 사용 가능한듯?
+      'tile-merged': isMerged, 
       'tile-new': isNew
       })}
     >
--- src/hook/useMoveTile.js
+++ src/hook/useMoveTile.js
@@ -3,9 +3,15 @@ import { addKeyObserver, removeKeyObserver } from "../util/keyboard";
 import { makeTile, moveTile } from "../util/tile";
 
 // UseEffect 
-export default function useMoveTile({ tileList, setTileList }) {
+export default function useMoveTile({ tileList, setTileList, setScore }) {
   function moveAndAdd({ x, y }) {
     const newTileList = moveTile({ tileList, x, y })
+    const score = newTileList.reduce(
+      (acc, item) => item.isMerged ? acc + item.value : acc
+      , 0
+    );
+    // 이전값에서 스코어를 더해준다
+    setScore(v => v + score);
     const newTile = makeTile(newTileList);
     newTile.isNew = true;
     newTileList.push(newTile);
@@ -24,16 +30,18 @@ export default function useMoveTile({ tileList, setTileList }) {
   function moveRight(){
     moveAndAdd({x: 1, y: 0});
   }
+  // mount 이후 한 번 실행되고
   useEffect(() => {
     addKeyObserver('up',moveUp);
     addKeyObserver('down',moveDown);
     addKeyObserver('left',moveLeft); 
     addKeyObserver('right',moveRight); 
+    // 리턴에 넣으면 unmount될 때 한 번 실행된다.
     return () => {
       removeKeyObserver('down',moveDown);
       removeKeyObserver('left',moveLeft); 
       removeKeyObserver('right',moveRight);
       removeKeyObserver('up',moveUp);
     }
-  })
+  }/* 이부분 미작성!!*/)
 };
